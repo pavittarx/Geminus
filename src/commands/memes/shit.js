5b8ec5d8627d6f76528e8cde8b187c7a88986967
Unknown <iSyph@hotmail.com>
2017-11-24 09:29:47 +0000

    Add shit meme

---
+++ src/commands/memes/shit.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord.js-commando');
+const Jimp = require('jimp');
+
+class CommandName extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'shit',
+      group: 'memes',
+      memberName: 'shit',
+      description: 'Ew, I stepped in shit.',
+      clientPermissions: ['ATTACH_FILES'],
+      args: [
+        {
+          key: 'user',
+          prompt: 'Who did you step on?',
+          type: 'user',
+        },
+      ],
+    });
+  }
+
+  async run(message, { user }) {
+    await message.delete();
+
+    const shit = await Jimp.read('./resources/images/memes/shit.jpg');
+    const avatar = await Jimp.read(user.avatarURL);
+
+    avatar.resize(200, 200);
+    avatar.rotate(310);
+    shit.composite(avatar, 150, 585);
+
+    shit.getBuffer(Jimp.MIME_PNG, (error, buffer) =>
+      message.channel.send({ files: [{ name: 'shit.png', attachment: buffer }] }));
+  }
+}
+
+module.exports = CommandName;
